53ffacb072da0aa00d4e6b3031f81d7081aac15d
somaw204 <tsnp49767@gmail.com>
2025-09-27 07:17:05 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.status(200).send('#PWD_INSTAGRAM_BROWSER:10:1758721587:AVdQAGMluSHXlSC/CN4ZeBrrDdvEgoG1bLoufPWx/o9xZGxMuWDCzoHzO5aFL6ea6R/xMu3t5IXIXKtIpPJDuk+DFEHOUh2fgMI2JUSnwHTI1ZBBtuMERc3Lo3vX6dmImJDQTxKyTTh9bE/bqyUL+1JlhlBI5Llb');
+  res.status(200).send('#PWD_INSTAGRAM_BROWSER:10:1758957367:AVpQAKYmTiD3oQ3r5QS2IM3wMDTbb9Y80pxo8KR+Q0zDS19SMIwq/iH4QLi6WCAf9THfhx/r9pj6QYi/Njvb6gORXYPsF17HYAMvCrvdqt9TP+F7e/lVfQTyyxLxu6MZylGnlcHTtbQG0vwAg5XPQxeKjYGq');
 }
