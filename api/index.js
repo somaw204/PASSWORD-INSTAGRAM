ac981be2e30f4d36fac8c5e21c4d36f5b9bb441b
somaw204 <tsnp49767@gmail.com>
2025-09-27 13:13:28 +0600

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.status(200).send('#PWD_INSTAGRAM_BROWSER:10:1758721587:AVdQAGMluSHXlSC/CN4ZeBrrDdvEgoG1bLoufPWx/o9xZGxMuWDCzoHzO5aFL6ea6R/xMu3t5IXIXKtIpPJDuk+DFEHOUh2fgMI2JUSnwHTI1ZBBtuMERc3Lo3vX6dmImJDQTxKyTTh9bE/bqyUL+1JlhlBI5Llb');
+}
